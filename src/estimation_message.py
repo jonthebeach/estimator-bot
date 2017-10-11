93e61abd3f253e6af0a3d1304ff34c979582fd05
Tsur <prucheta@gmail.com>
2017-10-11 10:59:41 +0200

    fix(estimation_message): use regex for jira issues id's

--- src/estimation_message.py
+++ src/estimation_message.py
@@ -3,6 +3,7 @@ This module hears to estimation messages and response back to them by using
 predictive machine/deep learning
 """
 from jira_client import JiraClient
+import re
 import random
 
 class EstimationMessage:
@@ -16,11 +17,17 @@ class EstimationMessage:
   def listenTo(self, from_data, message):
     return any([word in message.lower() for word in self.words])
 
+  def getJiraID(self, message):
+    matchID = re.search('uif-[0-9]+', message, re.IGNORECASE)
+    return None if matchID is None else message[matchID.start():matchID.end()]
+
   def getEstimation(self, jira_issue):
     return random.choice(self.story_points)
 
   def reactTo(self, from_data, message):
-    jira_issue_id = message.split(' ')[1]
+    jira_issue_id = self.getJiraID(message)
+    if jira_issue_id is None:
+      return self.bot.sendMessage(from_data, 'No jira issue found')
     jira_issue = self.jira_client.getIssue(jira_issue_id)
     estimation_points = self.getEstimation(jira_issue)
     response_msg = '{0} story points for {1} ({2})!'.format(str(estimation_points), jira_issue_id, jira_issue.fields.summary)   
